c10b0861ce74232b33da89733a68aa0bbdce791c
georgehampson-b6 <148126658+georgehampson-b6@users.noreply.github.com>
2024-03-26 13:34:46 +0000

    Update run_iperf_tcp.sh

--- run_iperf_tcp.sh
+++ run_iperf_tcp.sh
@@ -3,45 +3,37 @@
 now=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Get data from config file
-config_file="/home/bolt6/network_monitor/config.ini"
+config_file="config.ini"
 output_dir="network_monitor/iperf_results"
 output_file="$output_dir/iperf_results_$now.txt"
 
-# Get variables for IP & Port
-#!/bin/bash
+# Parse the location from [grand_rapids] section
+location=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^location/{print $2; exit}' "$config_file" | tr -d '[:space:]')
+ip=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^ip/{print $2; exit}' "$config_file" | tr -d '[:space:]')
+port=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^port/{print $2; exit}' "$config_file" | tr -d '[:space:]')
+streams=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^streams/{print $2; exit}' "$config_file" | tr -d '[:space:]')
+bandwidth=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^bandwidth/{print $2; exit}' "$config_file" | tr -d '[:space:]')
+seconds=$(awk -F' *= *' '/^\[las_vegas\]/{flag=1; next} /^\[/{flag=0} flag && /^seconds/{print $2; exit}' "$config_file" | tr -d '[:space:]')
 
-# Define the location to parse
-location="grand_rapids"
-
-# Use awk to parse the data for the specified location
-parsed_data=$(echo "$data" | awk -v location="$location" '/\['location'\]/,/\[' location '\]/')
-
-# Use awk to parse the data
-Location=$(echo "$data" | awk -F' = ' '/location/ {print $2}')
-IP=$(echo "$data" | awk -F' = ' '/ip/ {print $2}')
-Port=$(echo "$data" | awk -F' = ' '/port/ {print $2}')
-Streams=$(echo "$data" | awk -F' = ' '/streams/ {print $2}')
-Bandwidth=$(echo "$data" | awk -F' = ' '/bandwidth/ {print $2}')
-Time=$(echo "$data" | awk -F' = ' '/time/ {print $2}')
-
-# Print the parsed data
+# Output the parsed location
 echo "Location: $location"
-echo "IP: $IP"
-echo "Port: $Port"
-echo "Streams: $Streams"
-echo "Bandwidth: $Bandwidth"
-echo "Time: $Time"
+echo "IP: $ip"
+echo "Port: $port"
+echo "Streams: $streams"
+echo "Bandwidth: $bandwidth"
+echo "Time: $seconds"
 
 # Ping Google DNS 3 times
+
 ping -c 3 8.8.8.8
 
 # Check the return code of the ping command
 if [ $? -eq 0 ]; then
     echo "Ping successful. Running iperf test..."
     # Run iperf test here
-    iperf3 -c "$IP" -p "$Port" -P "$Streams" -b "$Bandwidth" -l 0  -t "$Time" >> "$output_file"
+    iperf3 -c "$ip" -p "$port" -P "$streams" -b "$bandwidth" -l 0  -t "$seconds" >> "$output_file"
 else
     echo "Ping failed. Exiting..."
 fi
 
-python network_monitor/parse_iperf.py "$output_file" "$Location" "$now"
+python network_monitor/parse_iperf.py "$output_file" "$location" "$now"
